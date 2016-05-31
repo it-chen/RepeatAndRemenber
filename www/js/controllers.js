4a44f16ed2d28004955cc2e485eb45f89b4cb9ae
it_chen <it_chen@126.com>
2016-05-31 18:57:32 +0800

    HTML

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('N2', function($scope, $ionicSideMenuDelegate,Units,$stateParams) {
+.controller('N2', function($scope, $ionicSideMenuDelegate,Units,$stateParams,$ionicPopup) {
 
   $scope.units = Units.all();
   $scope.totlePage = Units.totlePage();
+  $scope.abc = new Array();
+  for (i = 0; i < $scope.totlePage; i++) {
+    $scope.abc.push(i);
+  }
   $scope.unitNo = $stateParams.unitNo||0;
   $scope.itemNo = 1;
   $scope.item = Units.get($scope.unitNo,$scope.itemNo);
@@ -25,6 +29,28 @@ angular.module('starter.controllers', [])
       $scope.item = Units.get($scope.unitNo,$scope.itemNo);
     }
   }
+
+  $scope.showPopup = function() {
+  $scope.data = {}
+
+  // 自定义弹窗
+  var myPopup = $ionicPopup.show({
+   template:'<div class="card"><div class="item item-divider">卡片头</div>'+
+            '<div class="item item-text-wrap">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>'+
+            '<div class="item item-divider">卡片底</div></div>',
+   title: 'Enter Wi-Fi Password',
+   scope: $scope,
+   buttons: [
+     {
+       text: '<b>Save</b>',
+       type: 'button-positive',
+     },
+   ]
+  });
+  myPopup.then(function(res) {
+   console.log('Tapped!', res);
+  });
+  };
 })
 
 .controller('N1', function($scope) {
