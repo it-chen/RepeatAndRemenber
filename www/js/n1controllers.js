8faced9917f6a0bf9b90d5f26bcb9f47459da0a8
admin <it_chen@126.com>
2017-02-17 22:25:26 +0800

    new

---
+++ www/js/n1controllers.js
@@ -0,0 +1,67 @@
+angular.module('starter.controllers', [])
+
+.controller('N1', function($scope, $ionicSideMenuDelegate,N1Units,$stateParams,$ionicPopup) {
+
+  $scope.units = N1Units.all();
+  $scope.totlePage = N1Units.totlePage();
+  $scope.abc = new Array();
+  for (i = 0; i < $scope.totlePage; i++) {
+    $scope.abc.push(i);
+  }
+  $scope.unitNo = $stateParams.unitNo||0;
+  $scope.itemNo = 1;
+  $scope.item = N1Units.get($scope.unitNo,$scope.itemNo);
+
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  $scope.onDragUp = function(){
+    if(parseInt($scope.itemNo)<10){
+      $scope.itemNo = parseInt($scope.itemNo)+1;
+      $scope.item = N1Units.get($scope.unitNo,$scope.itemNo);
+    }
+  }
+
+  $scope.onDragDown = function(){
+    if(parseInt($scope.itemNo)>1){
+      $scope.itemNo = parseInt($scope.itemNo)-1;
+      $scope.item = N1Units.get($scope.unitNo,$scope.itemNo);
+    }
+  }
+
+  $scope.showPopup = function() {
+  $scope.data = {}
+
+  // 自定义弹窗
+  var myPopup = $ionicPopup.show({
+   template:'<div class="card"><div class="item item-divider">{{item.name}}</div>'+
+            '<div class="item item-text-wrap">中文：{{item.zhongwen}}<br/><br/>接续：{{item.jiefa}}<br/><br/>例句：{{item.liju}}<br/><br/>例句翻译：{{item.lijufanyi}}<br/><br/>'+
+            '<div ng-if="item.beizhu!=\'\'">备注：{{item.beizhu}}<br/></div></div>',
+   title: '详细解释',
+   scope: $scope,
+   buttons: [
+     {
+       text: '<b>懂了！</b>',
+       type: 'button-positive',
+     },
+   ]
+  });
+  myPopup.then(function(res) {
+   console.log('Tapped!', res);
+  });
+  };
+})
+
+.controller('N1', function($scope) {
+
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams) {
+})
+
+.controller('AboutCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
